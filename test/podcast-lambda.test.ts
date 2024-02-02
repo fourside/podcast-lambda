814ea81bf9625fada5e4cfb4fe6d5097f8c8748a
fourside <fourside@gmail.com>
2024-02-02 21:27:51 +0900

    Initial commit

---
+++ test/podcast-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PodcastLambda from '../lib/podcast-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/podcast-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PodcastLambda.PodcastLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
