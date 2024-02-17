74e6df9f0eb89c085238af7b614cfdc392778f3e
fourside <fourside@gmail.com>
2024-02-17 11:48:52 +0900

    remove deno script

--- app/src/output-filename.ts
+++
@@ -1,8 +0,0 @@
-import { format } from "std/datetime";
-
-export const FILE_DIR = "files";
-
-export function getOutputFilename(title: string, date: Date): string {
-  const suffix = format(date, "yyyyMMdd");
-  return `${FILE_DIR}/${title}-${suffix}.mp3`;
-}
