09fa73f4387b81225b6f81badff2209743dd1a54
fourside <fourside@gmail.com>
2024-04-04 11:31:21 +0900

    check if schemas satisfy existing types

--- lambda-image/src/main.ts
+++ lambda-image/src/main.ts
@@ -6,7 +6,7 @@ import { checkIfDurationIsShort } from "./duration-cheker";
 import { putMedia } from "./r2-client";
 import { record } from "./record";
 import { sendMessageToSlack } from "./slack-client";
-import type { CronEvent, SpotTaskEvent } from "./type";
+import type { CronEvent, EventDateTime, HourMin, SpotTaskEvent } from "./type";
 
 export async function main(event: unknown): Promise<void> {
   const result = v.safeParse(EventSchema, event);
@@ -66,35 +66,35 @@ async function spotTask(event: SpotTaskEvent): Promise<void> {
   }
 }
 
-const TimeSchema = v.object({
+const HourMinSchema = v.object({
   hour: v.number(),
   min: v.number(),
-});
+}) satisfies v.BaseSchema<HourMin>;
 
 const CronEventSchema = v.object({
   type: v.literal("cron"),
   title: v.string(),
   stationId: v.string(),
   personality: v.string(),
-  from: TimeSchema,
-  to: TimeSchema,
-});
+  from: HourMinSchema,
+  to: HourMinSchema,
+}) satisfies v.BaseSchema<CronEvent>;
 
-const DateTimeSchema = v.object({
+const EventDateTimeSchema = v.object({
   year: v.number(),
   month: v.number(),
   day: v.number(),
   hour: v.number(),
   min: v.number(),
-});
+}) satisfies v.BaseSchema<EventDateTime>;
 
 const SpotTaskEventSchema = v.object({
   type: v.literal("spot-task"),
   stationId: v.string(),
   title: v.string(),
   personality: v.string(),
-  from: DateTimeSchema,
-  to: DateTimeSchema,
-});
+  from: EventDateTimeSchema,
+  to: EventDateTimeSchema,
+}) satisfies v.BaseSchema<SpotTaskEvent>;
 
 const EventSchema = v.variant("type", [CronEventSchema, SpotTaskEventSchema]);
